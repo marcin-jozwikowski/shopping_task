96681c97eca0941b558f8cea54289f96a622d1cd
Marcin Jóźwikowski <marcin@jozwikowski.pl>
2020-03-03 22:06:51 +0100

    Sending email on added product + dockered MailCatcher

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
---
+++ src/MessageHandler/ProductAddedEmailSender.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\MessageHandler;
+
+
+use App\Message\ProductAddedMessage;
+use Swift_Mailer;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class ProductAddedEmailSender implements MessageHandlerInterface
+{
+    /** @var Swift_Mailer */
+    private $mailer;
+    /** @var string */
+    private $sender;
+    /** @var mixed */
+    private $recipient;
+
+    public function __construct(Swift_Mailer $mailer, ParameterBagInterface $params)
+    {
+        $this->mailer    = $mailer;
+        $this->sender    = $params->get('product_notifications.email.sender');
+        $this->recipient = $params->get('product_notifications.email.recipient');
+    }
+
+    public function __invoke(ProductAddedMessage $productAddedMessage)
+    {
+        $email = (new \Swift_Message('Product Added'))
+            ->setTo($this->recipient)
+            ->setFrom($this->sender)
+            ->addPart(
+                sprintf(
+                    "Product '%s' added by '%s'",
+                    $productAddedMessage->getProduct()->getName(),
+                    $productAddedMessage->getUser()->getUsername()
+                ),
+                'text/plain'
+            );
+
+        $this->mailer->send($email);
+    }
+}
